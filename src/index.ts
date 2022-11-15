3945b3cf135fc6047f6a91a64253e6609dd3b0b5
Alex0007 <Alex0007lolpvp@gmail.com>
2022-11-16 01:33:44 +0400

    fix formatter config

--- src/index.ts
+++ src/index.ts
@@ -2,92 +2,87 @@ import path from "path";
 import { performance, PerformanceObserver } from "perf_hooks";
 
 export interface TestOptions {
-	retriesNumber: number;
-	version: string;
+  retriesNumber: number;
+  version: string;
 }
 
 export interface RunOptions {
-	puppeteerVersions: string[];
+  puppeteerVersions: string[];
 }
 
 export interface ExecutionDetails {
-	functionName: string;
-	puppeteerVersion: string;
-	chromeVersion: string;
-	retryNumber: number;
-	casePath: string;
+  functionName: string;
+  puppeteerVersion: string;
+  chromeVersion: string;
+  retryNumber: number;
+  casePath: string;
 }
 
 export interface TestCasePerformanceResultItem {
-	details: ExecutionDetails;
-	item: PerformanceEntry;
+  details: ExecutionDetails;
+  item: PerformanceEntry;
 }
 
 export const testPuppeteerCase = async (
-	casePath: string,
-	options: TestOptions,
+  casePath: string,
+  options: TestOptions,
 ): Promise<TestCasePerformanceResultItem[]> => {
-	const testCase = path.resolve(casePath);
-	const testCaseFunction = require(testCase);
+  const testCase = path.resolve(casePath);
+  const testCaseFunction = require(testCase);
 
-	const measures: TestCasePerformanceResultItem[] = [];
+  const measures: TestCasePerformanceResultItem[] = [];
 
-	const puppeteer = require("puppeteer");
-	const puppeteerVersion = require("puppeteer/package.json").version;
+  const puppeteer = require("puppeteer");
+  const puppeteerVersion = require("puppeteer/package.json").version;
 
-	const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch();
 
-	const chromeVersion = await browser.version();
+  const chromeVersion = await browser.version();
 
-	await browser.close();
+  await browser.close();
 
-	const versionDetails: Pick<
-		ExecutionDetails,
-		"puppeteerVersion" | "chromeVersion"
-	> = {
-		puppeteerVersion,
-		chromeVersion,
-	};
+  const versionDetails: Pick<ExecutionDetails, "puppeteerVersion" | "chromeVersion"> = {
+    puppeteerVersion,
+    chromeVersion,
+  };
 
-	for (const retryNumber of new Array(options.retriesNumber)
-		.fill(null)
-		.map((_, index) => ++index)) {
-		const details: ExecutionDetails = {
-			functionName: testCaseFunction.name,
-			retryNumber,
-			casePath,
-			...versionDetails,
-		};
+  for (const retryNumber of new Array(options.retriesNumber).fill(null).map((_, index) => ++index)) {
+    const details: ExecutionDetails = {
+      functionName: testCaseFunction.name,
+      retryNumber,
+      casePath,
+      ...versionDetails,
+    };
 
-		const performanceObserver = new PerformanceObserver((list) => {
-			measures.push(
-				...list.getEntries().map((item) => {
-					return {
-						details,
-						item,
-					};
-				}),
-			);
-		});
+    const performanceObserver = new PerformanceObserver((list) => {
+      measures.push(
+        ...list.getEntries().map((item) => {
+          return {
+            details,
+            item,
+          };
+        }),
+      );
+    });
 
-		performanceObserver.observe({
-			buffered: true,
-			entryTypes: ["measure"],
-		});
+    performanceObserver.observe({
+      buffered: true,
+      entryTypes: ["measure"],
+    });
 
-		performance.mark(`execution-start`);
-		await testCaseFunction();
-		performance.mark(`execution-finish`);
+    performance.mark(`execution-start`);
+    await testCaseFunction();
+    performance.mark(`execution-finish`);
 
-		performance.measure("execution", "execution-start", "execution-finish");
+    performance.measure("execution", "execution-start", "execution-finish");
 
-		// We need to wait for observer callback to be completed before disconnecting
-		await new Promise((resolve) => {
-			setImmediate(resolve);
-		});
+    // We need to wait for observer callback to be completed before disconnecting
+    await new Promise((resolve) => {
+      setImmediate(resolve);
+    });
 
-		performanceObserver.disconnect();
-	}
+    performanceObserver.disconnect();
+  }
 
-	return measures;
+  return measures;
 };
--- test-cases/generate-pdf.js
+++ test-cases/generate-pdf.js
@@ -1,33 +1,31 @@
+const os = require("os");
+const path = require("path");
+const { performance } = require("perf_hooks");
 
-const os = require('os')
-const path = require('path');
-const { performance } = require('perf_hooks');
-
-const puppeteer = require('puppeteer');
-const info = require('puppeteer/package.json')
-
+const puppeteer = require("puppeteer");
+const info = require("puppeteer/package.json");
 
 const generatePdf = async () => {
-    const puppeteerVersion = info.version
-    const resultPath = path.resolve(os.tmpdir(), `result-${puppeteerVersion}-${+new Date()}.pdf`)
+  const puppeteerVersion = info.version;
+  const resultPath = path.resolve(os.tmpdir(), `result-${puppeteerVersion}-${+new Date()}.pdf`);
 
-    performance.mark('browser-launch-start')
-    const browser = await puppeteer.launch()
-    performance.mark('browser-launch-finish')
+  performance.mark("browser-launch-start");
+  const browser = await puppeteer.launch();
+  performance.mark("browser-launch-finish");
 
-    performance.measure('browser-launch', 'browser-launch-start', 'browser-launch-finish')
+  performance.measure("browser-launch", "browser-launch-start", "browser-launch-finish");
 
-    const page = await browser.newPage()
+  const page = await browser.newPage();
 
-    await page.goto('http://example.com/')
+  await page.goto("http://example.com/");
 
-    await page.pdf({
-        path: resultPath
-    })
+  await page.pdf({
+    path: resultPath,
+  });
 
-    console.log('Saved pdf to', resultPath);
+  console.log("Saved pdf to", resultPath);
 
-    await browser.close()
-}
+  await browser.close();
+};
 
 module.exports = generatePdf;
