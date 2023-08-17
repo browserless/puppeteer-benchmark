0162992f413e9bf91580e85423294f1e6284a4f4
Anderson Martínez <anderson@browserless.io>
2023-08-17 15:42:03 -0400

    fix: various fixes
    - minifying CSS on HTML report
    - fixing memory message for windows
    - fixing README's TOC's hrefs for github

--- src/utils.ts
+++ src/utils.ts
@@ -108,81 +108,7 @@ const resultsToHTMLTable = (
 	table += "</table>";
 
 	return `<html>
-  <style>
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-  thead tr td {
-    border-top: none !important;
-  }
-  .table {
-  width: 100%;
-  margin-bottom: 1rem;
-  color: #212529;
-  }
-
-  .table th, .table td {
-  padding: 0.75rem;
-  vertical-align: top;
-  border-top: 1px solid #dee2e6;
-  }
-
-  body {
-  font-family: "Roboto", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  background-color: #fefefe;
-  display: flex;
-  justify-content: center;
-  margin-top: 8em;
-  }
-
-  p {
-  color: #b3b3b3;
-  font-weight: 300; }
-
-  a {
-  -webkit-transition: .3s all ease;
-  -o-transition: .3s all ease;
-  transition: .3s all ease; }
-  a, a:hover {
-  text-decoration: none !important; }
-
-  .content {
-  padding: 7rem 0; }
-
-  h2 {
-  font-size: 20px;
-  margin-bottom: 2em;
-  }
-
-  .custom-table {
-  min-width: 900px; }
-  .custom-table thead tr, .custom-table thead th {
-  border-top: none;
-  border-bottom: none !important; }
-  .custom-table tbody th, .custom-table tbody td {
-  color: #777 !important;
-  padding-right: 30px;
-  font-weight: 300;
-  }
-
-  td div {
-    padding: 2px 6px;
-    width: fit-content !important;
-  }
-
-  .highlight-high {
-    background: #ffe6e6;
-    border-radius: 3px;
-    color: #b55d5d;
-  }
-
-
- .highlight-low {
-    background: #ecf2eb !important;
-    border-radius: 3px;
-    color: #267135;
-  }
-
-
-  </style>
+  <style>@import url(https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap);thead tr td{border-top:none!important}.table{width:100%;margin-bottom:1rem;color:#212529}.table td,.table th{padding:.75rem;vertical-align:top;border-top:1px solid #dee2e6}body{font-family:Roboto,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";background-color:#fefefe;display:flex;justify-content:center;margin-top:8em}p{color:#b3b3b3;font-weight:300}a{-webkit-transition:.3s all ease;-o-transition:.3s all ease;transition:.3s all ease}a,a:hover{text-decoration:none!important}.content{padding:7rem 0}h2{font-size:20px;margin-bottom:2em}.custom-table{min-width:900px}.custom-table thead th,.custom-table thead tr{border-top:none;border-bottom:none!important}.custom-table tbody td,.custom-table tbody th{color:#777!important;padding-right:30px;font-weight:300}td div{padding:2px 6px;width:fit-content!important}.highlight-high{background:#ffe6e6;border-radius:3px;color:#b55d5d}.highlight-low{background:#ecf2eb!important;border-radius:3px;color:#267135}</style>
   <div style="width: 85%">
   <div style="display: flex;">
     <img
@@ -193,7 +119,11 @@ const resultsToHTMLTable = (
   </div>
   ${table}
   <br />
-  <p>* This report was generated on Windows, therefore CPU measurements are unreliable.</p>
+  ${
+		process.platform === "win32"
+			? "<p>* This report was generated on Windows, therefore CPU measurements are unreliable.</p>"
+			: ""
+	}
   <br /><br />
   </div>
   </html>`;
